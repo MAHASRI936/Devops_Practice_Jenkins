f448ecc42f690242e3d93548efda5eaa918956c7
MahasriSaravanan <mahasris.23csd@kongu.edu>
2026-03-16 15:10:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "game_server" {
-  ami           = "ami-019715e0d74f695be"
+  ami           = "ami-0a14f53a6fe4dfcd1"
   instance_type = "t3.small"
-  key_name      = "dev"
+  key_name      = "key"
 
   security_groups = [aws_security_group.boxGame.name]
 
